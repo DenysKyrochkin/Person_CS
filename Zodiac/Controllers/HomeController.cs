73dc3d95362e3425f88580b8e4530f4a17d4f1ec
Denys <denys@MacBook-Pro-Denys.local>
2020-04-11 19:43:55 +0300

    release v2

---
+++ Zodiac/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Zodiac.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zodiac.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result()
+        {
+            Person person = new Person(
+                HttpContext.Request.Form["Name"],
+                HttpContext.Request.Form["Surname"],
+                HttpContext.Request.Form["Email"],
+                new Dates(HttpContext.Request.Form["Date"])
+                );
+
+            //if(HttpContext.Request.Form == null)
+            //{
+
+            //}
+            if (person.Birthday.Age > 135 || person.Birthday.Age < 0)
+            {
+                return Redirect("/Home/Index");
+            }
+            if (person.IsBirthday)
+            {
+                ViewBag.Whishes = "Happy Birthday to you!";
+            }
+
+            return View(person);
+        }
+
+   
+    }
+}
+
+
---
+++ Zodiac/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Zodiac.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public Dates Birthday { get; set; }
+
+        public bool IsAdult => Birthday.Age >= 18;
+        public string SunSign => Birthday.ZodiacW;
+        public string ChineseSign => Birthday.ZodiacC;
+        public bool IsBirthday => Birthday.isBirthday();
+
+        public Person(string name, string surname, string email, Dates birthday)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Birthday = birthday;
+        }
+
+        public Person(string name, string surname, string email)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Birthday = default;
+        }
+
+        public Person(string name, string surname, Dates birthday)
+        {
+            Name = name;
+            Surname = surname;
+            Birthday = birthday;
+            Email = default;
+        }
+    }
+}
